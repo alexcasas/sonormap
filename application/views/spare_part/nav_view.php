5662f3be49642ef953d2142ce7bd154ee8cc9f4a
thebluemax <thebluemax13@gmail.com>
2013-05-14 02:17:32 +0200

    mejoras. agregados

--- application/views/spare_part/nav_view.php
+++ application/views/spare_part/nav_view.php
@@ -1,6 +1,6 @@
 <ul>
-	<li><a href="/sonormap/index.php/inici" title="">INICI</a></li>
-	<li><a href="/sonormap/index.php/inici" title="">ARXIU</a></li>
-	<li><a href="/sonormap/index.php/inici" title="">MAPS</a></li>
-	<li><a href="/sonormap/index.php/registrar" title="">REGISTRAR-SE</a></li>
+	<li><a href=<?='"'.site_url('inici').'"'?> title="">INICI</a></li>
+	<li><a href=<?='"'.site_url('arxiu').'"'?> title="">ARXIU</a></li>
+	<li><a href=<?='"'.site_url('maps').'"'?> title="">MAPS</a></li>
+	<li><a href=<?='"'.site_url('registrar').'"'?> title="">MI PERFIL</a></li>
 </ul>
