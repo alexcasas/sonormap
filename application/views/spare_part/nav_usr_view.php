d24ce8ad8584c24ea08f1b20566b9dca4d5e09ef
thebluemax <thebluemax13@gmail.com>
2013-05-15 03:05:24 +0200

    navegador actualizado

--- application/views/spare_part/nav_usr_view.php
+++ application/views/spare_part/nav_usr_view.php
@@ -3,6 +3,6 @@
 	<li><a href=<?='"'.site_url('arxiu').'"'?> title="">ARXIU</a></li>
 	<li><a href=<?='"'.site_url('maps').'"'?> title="">MAPS</a></li>
 	<li><a href=<?='"'.site_url('user/perfil').'"'?> title="">MI PERFIL</a></li>
-	<li><a href=<?='"'.site_url('desar').'"'?> title="">ARCHIVAR DOCUMENTO</a></li>
+	<li><a href=<?='"'.site_url('arxivar').'"'?> title="">ARCHIVAR DOCUMENTO</a></li>
 	<li><a href=<?='"'.site_url('registrar').'"'?> title="">REGISTRAR-SE</a></li>
 </ul>
